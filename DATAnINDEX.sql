1ae3ca84f9dfc51b09d2e4db0e0380cccd890fb7
Chaoran Huang <chaoran.huang@unsw.edu.au>
2015-09-17 19:35:47 +1000

    local xml loading and insert data to tables, with indexes construction. 16 Sept.,2015

---
+++ DATAnINDEX.sql
@@ -0,0 +1,49 @@
+# Chaoran Huang 2015 at UNSW CSE
+# Referenced Georgios Gousios 2013 code on GitHub
+# MIT-licensed
+
+LOAD XML LOCAL INFILE 'Badges.xml'
+INTO TABLE Badges
+ROWS IDENTIFIED BY '<row>';
+
+LOAD XML LOCAL INFILE 'Comments.xml'
+INTO TABLE Comments
+ROWS IDENTIFIED BY '<row>';
+
+LOAD XML LOCAL INFILE 'PostHistory.xml'
+INTO TABLE Post_history
+ROWS IDENTIFIED BY '<row>';
+
+LOAD XML LOCAL INFILE 'PostLinks.xml'
+INTO TABLE Post_links
+ROWS IDENTIFIED BY '<row>';
+
+LOAD XML LOCAL INFILE 'Posts.xml'
+INTO TABLE Posts
+ROWS IDENTIFIED BY '<row>';
+
+LOAD XML LOCAL INFILE 'Tags.xml'
+INTO TABLE Tags
+ROWS IDENTIFIED BY '<row>';
+
+LOAD XML LOCAL INFILE 'Users.xml'
+INTO TABLE Users
+ROWS IDENTIFIED BY '<row>';
+
+LOAD XML LOCAL INFILE 'Votes.xml'
+INTO TABLE Votes
+ROWS IDENTIFIED BY '<row>';
+
+CREATE INDEX badges_idx_1 ON badges(UserId);
+CREATE INDEX Comments_idx_1 ON Comments(PostId);
+CREATE INDEX Comments_idx_2 ON Comments(UserId);
+
+CREATE INDEX Post_history_idx_1 ON Post_history(PostId);
+CREATE INDEX Post_history_idx_2 ON Post_history(UserId);
+
+CREATE INDEX Posts_idx_1 ON Posts(AcceptedAnswerId);
+CREATE INDEX Posts_idx_2 ON Posts(ParentId);
+CREATE INDEX Posts_idx_3 ON Posts(OwnerUserId);
+CREATE INDEX Posts_idx_4 ON Posts(LastEditorUserId);
+
+CREATE INDEX Votes_idx_1 ON Votes(PostId);
