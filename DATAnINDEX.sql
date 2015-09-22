ff953050f2d54a9a9831cc979212aeb22384b54d
Chaoran Huang <chaoran.huang@unsw.edu.au>
2015-09-22 10:52:17 +1000

    sql script for data importing and index creation

--- DATAnINDEX.sql
+++ DATAnINDEX.sql
@@ -2,35 +2,35 @@
 # Referenced Georgios Gousios 2013 code on GitHub
 # MIT-licensed
 
-LOAD XML LOCAL INFILE 'Badges.xml'
+LOAD XML LOCAL INFILE '/Users/chaoran/Documents/Work/stackExchange/data/stackoverflow.com/Badges.xml'
 INTO TABLE Badges
 ROWS IDENTIFIED BY '<row>';
 
-LOAD XML LOCAL INFILE 'Comments.xml'
+LOAD XML LOCAL INFILE '/Users/chaoran/Documents/Work/stackExchange/data/stackoverflow.com/Comments.xml'
 INTO TABLE Comments
 ROWS IDENTIFIED BY '<row>';
 
-LOAD XML LOCAL INFILE 'PostHistory.xml'
+LOAD XML LOCAL INFILE '/Users/chaoran/Documents/Work/stackExchange/data/stackoverflow.com/PostHistory.xml'
 INTO TABLE Post_history
 ROWS IDENTIFIED BY '<row>';
 
-LOAD XML LOCAL INFILE 'PostLinks.xml'
+LOAD XML LOCAL INFILE '/Users/chaoran/Documents/Work/stackExchange/data/stackoverflow.com/PostLinks.xml'
 INTO TABLE Post_links
 ROWS IDENTIFIED BY '<row>';
 
-LOAD XML LOCAL INFILE 'Posts.xml'
+LOAD XML LOCAL INFILE '/Users/chaoran/Documents/Work/stackExchange/data/stackoverflow.com/Posts.xml'
 INTO TABLE Posts
 ROWS IDENTIFIED BY '<row>';
 
-LOAD XML LOCAL INFILE 'Tags.xml'
+LOAD XML LOCAL INFILE '/Users/chaoran/Documents/Work/stackExchange/data/stackoverflow.com/Tags.xml'
 INTO TABLE Tags
 ROWS IDENTIFIED BY '<row>';
 
-LOAD XML LOCAL INFILE 'Users.xml'
+LOAD XML LOCAL INFILE '/Users/chaoran/Documents/Work/stackExchange/data/stackoverflow.com/Users.xml'
 INTO TABLE Users
 ROWS IDENTIFIED BY '<row>';
 
-LOAD XML LOCAL INFILE 'Votes.xml'
+LOAD XML LOCAL INFILE '/Users/chaoran/Documents/Work/stackExchange/data/stackoverflow.com/Votes.xml'
 INTO TABLE Votes
 ROWS IDENTIFIED BY '<row>';
 
@@ -45,5 +45,6 @@ CREATE INDEX Posts_idx_1 ON Posts(AcceptedAnswerId);
 CREATE INDEX Posts_idx_2 ON Posts(ParentId);
 CREATE INDEX Posts_idx_3 ON Posts(OwnerUserId);
 CREATE INDEX Posts_idx_4 ON Posts(LastEditorUserId);
+CREATE INDEX Posts_idx_5 ON Posts(PostTypeId);
 
 CREATE INDEX Votes_idx_1 ON Votes(PostId);
