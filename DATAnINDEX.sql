afaf49335e337f781f742e7415f3fb40b950e36a
Chaoran Huang <chaoran.huang@unsw.edu.au>
2015-09-24 17:43:01 +1000

    data filling: q/a p/n-votes

--- DATAnINDEX.sql
+++ DATAnINDEX.sql
@@ -34,6 +34,44 @@ LOAD XML LOCAL INFILE '/Users/chaoran/Documents/Work/stackExchange/data/stackove
 INTO TABLE Votes
 ROWS IDENTIFIED BY '<row>';
 
+
+INSERT INTO Questions(Id, AcceptedAnswerId, Score, ViewCount, 
+            Body, OwnerUserId, LastEditorUserId, 
+            LastEditDate, LastActivityDate, Title,
+            Tags, AnswerCount, CommentCount, 
+            FavoriteCount, CreationDate, CommunityOwnedDate, 
+            LastEditorDisplayName, OwnerDisplayName)
+
+  SELECT  Id, AcceptedAnswerId, Score, ViewCount, 
+          Body, OwnerUserId, LastEditorUserId, 
+          LastEditDate, LastActivityDate, Title,
+          Tags, AnswerCount, CommentCount, 
+          FavoriteCount, CreationDate, CommunityOwnedDate, 
+          LastEditorDisplayName, OwnerDisplayName
+  FROM Posts WHERE PostTypeId = 1 ;
+
+INSERT INTO Answers(Id, ParentId, Score, Body, 
+            OwnerUserId,LastEditorUserId, LastEditDate, 
+            LastActivityDate, Title, Tags, CommentCount, 
+            FavoriteCount, CreationDate, CommunityOwnedDate, 
+            LastEditorDisplayName, OwnerDisplayName)
+
+  SELECT  Id, ParentId, Score, Body, 
+          OwnerUserId,LastEditorUserId, LastEditDate, 
+          LastActivityDate, Title, Tags, CommentCount, 
+          FavoriteCount, CreationDate, CommunityOwnedDate, 
+          LastEditorDisplayName, OwnerDisplayName
+  FROM Posts WHERE PostTypeId = 2 ;
+
+INSERT INTO PostiveVotes(Id, PostId, CreationDate, UserId, BountyAmount)
+  SELECT  Id, PostId, CreationDate, UserId, BountyAmount
+  FROM Votes WHERE VoteTypeId = 1 or VoteTypeId = 2 or VoteTypeId = 5 ;
+  
+INSERT INTO NegativeVotes(Id, PostId, CreationDate, UserId, BountyAmount)
+  SELECT  Id, PostId, CreationDate, UserId, BountyAmount
+  FROM Votes WHERE  VoteTypeId = 3 or VoteTypeId = 4 or VoteTypeId = 6 or 
+                    VoteTypeId = 10 or VoteTypeId = 12 or VoteTypeId = 15 ;
+
 CREATE INDEX badges_idx_1 ON badges(UserId);
 CREATE INDEX Comments_idx_1 ON Comments(PostId);
 CREATE INDEX Comments_idx_2 ON Comments(UserId);
