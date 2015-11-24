17e1b18bdb28ac8d832a6ab3f97a72d5a9f5f030
Chaoran Huang <chaoran.huang@unsw.edu.au>
2015-11-24 17:01:38 +1100

    create feature tabel for computation

--- featureTable.sql
+++ featureTable.sql
@@ -1,31 +1,33 @@
-# Chaoran Huang 2015 at UNSW CSE
+# Chaoran Huang 2DEFAULT '0'15 at UNSW CSE
 # MIT-licensed
 
 use stackoverflow;
 
 CREATE TABLE FeaturesDescription (
     Id INT NOT NULL PRIMARY KEY,
-    Name VARCHAR(60) NULL,
+    Name VARCHAR(6DEFAULT '0') NULL,
     Description Text
 );
 
 Create table UsersFeatures(
-  Id INT NOT NULL PRIMARY KEY,
+  Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   UserId INT NOT NULL,
-  FeatureValue DOUBLE NULL,
-  NormalizedValue DOUBLE NULL
+  FeatureId INT NOT NULL,
+  FeatureValue DOUBLE DEFAULT '0',
+  NormalizedValue DOUBLE DEFAULT '0'
 );
 
 Create table PostFeatures(
-  Id INT NOT NULL PRIMARY KEY,
-  UserId INT NOT NULL,
-  FeatureValue DOUBLE NULL,
-  NormalizedValue DOUBLE NULL
+  Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  PostId INT NOT NULL,
+  FeatureId INT NOT NULL,
+  FeatureValue DOUBLE DEFAULT '0',
+  NormalizedValue DOUBLE DEFAULT '0'
 );
 
 Create table OtherFeatures(
-  Id INT NOT NULL PRIMARY KEY,
-  UserId INT NOT NULL,
-  FeatureValue DOUBLE NULL,
-  NormalizedValue DOUBLE NULL
+  Id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  FeatureId INT NOT NULL,
+  FeatureValue DOUBLE DEFAULT '0',
+  NormalizedValue DOUBLE DEFAULT '0'
 );
